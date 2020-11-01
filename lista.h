e1b3c7c295ee580b2894cf4480762806944ced42
joeldevel <christianjoel.developer@gmail.com>
2020-11-01 04:23:56 -0300

    Pre and Post conditions added into lista.h

---
+++ lista.h
@@ -0,0 +1,85 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <stdbool.h>
+
+
+/* ******************************************************************
+ *                DEFINICION DE LOS TIPOS DE DATOS
+ * *****************************************************************/
+
+/* La lista está planteada como una lista de punteros genéricos. */
+
+struct lista;
+typedef struct lista lista_t;
+
+
+/* ******************************************************************
+ *                    PRIMITIVAS DE LA PILA
+ * *****************************************************************/
+
+// Crea una lista.
+// Post: devuelve una nueva lista vacía.
+lista_t *lista_crear(void);
+
+// Devuelve verdadero si la lista no tiene elementos enlistados, false en caso contrario.
+// Pre: la lista fue creada.
+bool lista_esta_vacia(const lista_t *lista);
+
+// Inserta un elemento al principio de la lista
+// Pre: la lista fue creada
+// Post: la lista contiene el elemento pasado como argumento al principio
+// retorna true si se pudo insertar, caso contrario false
+bool lista_insertar_primero(lista_t *lista, void *dato);
+
+// Inserta un elemento al final de la lista
+// Pre: la lista fue creada
+// Post: la lista contiene el elemento pasado como argumento al final
+// retorna true si se pudo insertar, caso contrario false
+bool lista_insertar_ultimo(lista_t *lista, void *dato);
+
+// Saca el primer elemento de la lista. Si la lista tiene elementos, se quita el
+// primero de la lista, y se devuelve su valor, si está vacía, devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el valor del primer elemento anterior, la lista
+// contiene un elemento menos, si la lista no estaba vacía.
+void *lista_borrar_primero(lista_t *lista);
+
+// Obtiene el valor del primer elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el primer elemento de la lista, cuando no está vacía.
+void *lista_ver_primero(const lista_t *lista);
+
+// Obtiene el valor del último elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del último, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el último elemento de la lista, cuando no está vacía.
+void *lista_ver_ultimo(const lista_t* lista);
+
+// Devuelve el tamaño de la lista
+// Pre: la lista fue creada
+size_t lista_largo(const lista_t *lista);
+
+// Destruye la lista. Si se recibe la función destruir_dato por parámetro,
+// para cada uno de los elementos de la lista llama a destruir_dato.
+// Pre: la lista fue creada. destruir_dato es una función capaz de destruir
+// los datos de la lista, o NULL en caso de que no se la utilice.
+// Post: se eliminaron todos los elementos de la lista.
+void lista_destruir(lista_t *lista, void (*destruir_dato)(void *));
+
+/* *****************************************************************
+ *                      PRUEBAS UNITARIAS
+ * *****************************************************************/
+
+// Realiza pruebas sobre la implementación realizada.
+//
+// Las pruebas deben emplazarse en el archivo ‘pruebas_cola.c’, y
+// solamente pueden emplear la interfaz pública tal y como aparece en cola.h
+// (esto es, las pruebas no pueden acceder a los miembros del struct cola).
+//
+// Para la implementación de las pruebas se debe emplear la función
+// print_test(), como se ha visto en TPs anteriores.
+void pruebas_cola_estudiante(void);
+
+#endif  // LISTA_H
